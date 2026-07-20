cce3a9baa4691c86da131f562dd20132277a59e1
Neeesa <vanessazj1@gmail.com>
2026-07-20 23:51:35 +0800

    Update SupabaseServerClient.js

--- backend/src/config/SupabaseServerClient.js
+++ backend/src/config/SupabaseServerClient.js
@@ -1,13 +1,11 @@
-const { createClient } = require('@supabase/supabase-js');
+import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = process.env.SUPABASE_URL || process.env.Database_URL;
 const supabaseKey = process.env.SUPABASE_SERVICE_KEY || process.env.Database_Anon_Key;
 
-const supabaseServer = createClient(supabaseUrl, supabaseKey, {
+export const supabaseServer = createClient(supabaseUrl, supabaseKey, {
   auth: { 
     persistSession: false, 
     autoRefreshToken: false 
   }
 });
-
-module.exports = { supabaseServer };
