97d36df89814ab78b778b578be94ebfeacf9acbf
Neeesa <vanessazj1@gmail.com>
2026-07-20 22:24:09 +0800

    Update supabase.ts

--- mobile-app/src/lib/supabase.ts
+++ mobile-app/src/lib/supabase.ts
@@ -2,9 +2,8 @@ import 'react-native-url-polyfill/auto';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { createClient } from '@supabase/supabase-js';
 
-// Ensure these environment variables are set in your mobile project
-const SupabaseUrl = process.env.Database_URL!;
-const SupabaseAnonKey = process.env.Database_Anon_Key!;
+const SupabaseUrl = process.env.SUPABASE_URL || process.env.Database_URL || '';
+const SupabaseAnonKey = process.env.SUPABASE_SERVICE_KEY || process.env.Database_Anon_Key || '';
 
 export const supabaseMobile = createClient(SupabaseUrl, SupabaseAnonKey, {
   auth: {
