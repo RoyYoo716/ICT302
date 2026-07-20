84841b0f87906e08ae015a03f10ccbc876f93a2f
Fawnstie <mine1cow@gmail.com>
2026-07-20 21:38:21 +0800

    Update and rename app.mjs to app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 const path = require('path');
 
 const app = express();
-const verifySecuritySession = require('./middleware/SecurityAuth');
+const verifySecuritySession = require('./middleware/SecurityAuth.mjs');
 
 app.use(cors());
 app.use(express.json());
