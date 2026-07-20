6d09df607deeef63fb0ac78371a24eb73093914a
Neeesa <vanessazj1@gmail.com>
2026-07-20 22:22:33 +0800

    Update SupabaseWebClient.js

--- admin-web/src/utils/SupabaseWebClient.js
+++ admin-web/src/utils/SupabaseWebClient.js
@@ -1,6 +1,9 @@
 import { createClient } from '@supabase/supabase-js';
 
-export const supabaseWeb = createClient(process.env.Database_URL, process.env.Database_Anon_Key, {
+const supabaseUrl = process.env.SUPABASE_URL || process.env.Database_URL;
+const supabaseKey = process.env.SUPABASE_SERVICE_KEY || process.env.Database_Anon_Key;
+
+export const supabaseWeb = createClient(supabaseUrl, supabaseKey, {
   auth: { 
     persistSession: true, 
     autoRefreshToken: true 
